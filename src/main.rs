2632e50fd965247640166da999a967514c39d946
朱重迁 <z32664206862022@163.com>
2026-02-16 10:44:02 +0800

    feat:集成color-eyre以增强错误处理，并添加ratatui以进行UI渲染
    -添加了“color-eyre”依赖项，以改进错误报告。
    -引入了用于终端UI渲染的“ratatui”，包括一个列表小部件。
    -实现了导航和退出UI的关键事件处理。
    -重构日志记录，以包括按键事件。
    -清理了`dir_listing.rs`中未使用的导入。

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,21 @@ pub mod utils;
 pub use dir_listing::{list_directory, search_and_display_interactive};
 
 use clap::Parser;
-use log::info;
 use logger::init_logger;
 use std::path::Path;
 
+use crossterm::event::{self, KeyCode};
 use models::Cli;
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::style::{Color, Modifier, Style, Stylize};
+use ratatui::text::{Line, Span};
+use ratatui::widgets::{List, ListDirection, ListState};
+use ratatui::Frame;
 
 fn main() -> Result<(), anyhow::Error> {
     init_logger();
+    let _ = init_render();
+
     let start_time = std::time::Instant::now();
     let args = Cli::parse();
     let path = Path::new(&args.dir);
@@ -40,6 +47,59 @@ fn main() -> Result<(), anyhow::Error> {
     //     );
     // }
     Ok(())
+}
+
+fn init_render() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let mut list_state = ListState::default().with_selected(Some(0));
+    ratatui::run(|terminal| loop {
+        terminal.draw(|frame| render(frame, &mut list_state))?;
+        if let event::Event::Key(key) = event::read()? {
+            // 仅处理按下事件
+            if key.kind == event::KeyEventKind::Press {
+                log::info!("Key pressed: {:?}", key.code);
+                match key.code {
+                    KeyCode::Char('j') | KeyCode::Down => list_state.select_next(),
+                    KeyCode::Char('k') | KeyCode::Up => list_state.select_previous(),
+                    KeyCode::Char('q') | KeyCode::Esc => break Ok(()),
+                    _ => {}
+                }
+            }
+        }
+    })
+}
+
+/// Render the UI with various lists.
+fn render(frame: &mut Frame, list_state: &mut ListState) {
+    let constraints = [
+        Constraint::Length(1),
+        Constraint::Fill(1),
+        Constraint::Length(1),
+    ];
+    let layout = Layout::vertical(constraints).spacing(1);
+    let [top, first, second] = frame.area().layout(&layout);
+
+    let title = Line::from_iter([
+        Span::from("List Widget").bold(),
+        Span::from(" (Press 'q' to quit and arrow keys to navigate)"),
+    ]);
+    frame.render_widget(title.centered(), top);
+
+    render_list(frame, first, list_state);
+}
+
+/// Render a list.
+pub fn render_list(frame: &mut Frame, area: Rect, list_state: &mut ListState) {
+    let items = [
+        "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9",
+    ];
+    let list = List::new(items)
+        .style(Color::White)
+        .highlight_style(Modifier::REVERSED)
+        .highlight_symbol("> ");
+
+    frame.render_stateful_widget(list, area, list_state);
 }
 
 #[cfg(test)]
